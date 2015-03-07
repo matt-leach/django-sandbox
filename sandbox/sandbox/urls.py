af57df85644077e140d4321d4e7fe3eda4af13bf
Matt Leach <matt.leach25@gmail.com>
2015-03-07 09:17:38 +0000

    Adding a view to show what request.GET is

--- sandbox/sandbox/urls.py
+++ sandbox/sandbox/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponse
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +8,8 @@ urlpatterns = patterns('',
     url(r'^bootstrap/', include('bootstrap.urls')),
     url(r'^location/', include('location.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^whatsget/', 'sandbox.urls.printget')
 )
+
+def printget(request):
+    return HttpResponse(request.GET.items())
